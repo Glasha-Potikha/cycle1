7a79b963a3cf3a600fcc06024bf9ea8b84c65cec
Glashaty <firatiha7404@gmail.com>
2024-10-30 18:51:45 +0300

    Дз-шка, доработанная

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         //Задача №1
-        for (int i = 0; i <= 10; i++) {
+        for (int i = 0; i < 10; i++) {
             System.out.println(i);
         }
         //Задача №2
-        for (int i = 10; i >= 0; i--) {
+        for (int i = 10; i > 1; i--) {
             System.out.println(i);
         }
         //Задача №3
@@ -18,7 +18,7 @@ public class Main {
         }
         //Задача №5
         for (int i = 1904; i <= 2096; i += 4) {
-            System.out.println(i);
+            System.out.println(i + " год является високосным");
         }
         //Задача №6
         for (int i = 7; i <= 98; i += 7) {
@@ -30,19 +30,19 @@ public class Main {
         }
         //Задача №8
         int savings = 0;
-        for (int i = 0; i <= 12; i++) {
+        for (int i = 0; i < 12; i++) {
             savings += 29000;
             System.out.println("Месяц " + i + ", сумма накоплений равна " + savings + " рублей");
         }
         //Задача №9
         int savingsInTheBank = 0;
-        for (int i = 0; i <= 12; i++) {
+        for (int i = 0; i < 12; i++) {
             savingsInTheBank = savingsInTheBank + (savingsInTheBank / 100);
             savingsInTheBank += 29000;
             System.out.println("Месяц " + i + ", сумма накоплений равна " + savingsInTheBank + " рублей");
         }
         //Задача №10
-        for (int i = 0; i <= 10; i++) {
+        for (int i = 1; i <= 10; i++) {
             System.out.println("2*" + i + "=" + i * 2);
         }
     }
